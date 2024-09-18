653d3f0e0120e2677b1546accaa43e4371e25267
dauzkepow <dauzcool20@gmail.com>
2024-09-18 12:05:41 +0700

    Model LaguDaerah

--- app/Models/laguDaerah.php
+++ app/Models/laguDaerah.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class laguDaerah extends Model
+class LaguDaerah extends Model
 {
     use HasFactory;
     protected $fillable = ['judul', 'lagu', 'daerah'];
